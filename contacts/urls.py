87d7833e4e74ae7e79c4e0f997a780842ba43983
Jasmine Massey <jazminemazzey@gmail.com>
2022-06-24 13:44:46 +0530

    updated urls accordingly

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,10 +1,23 @@
 import imp
 from django.urls import path
-from .views import IndexView
+from .views import (
+        IndexView,
+        ContactDetailView, 
+        ContactCreateView, 
+        ManageContactList,
+        ContactUpdateView,
+        ContactDeleteView
+        )    
 
 
 app_name = 'contacts'
 
 urlpatterns = [
     path('',IndexView.as_view(), name="home"),
+    path('contact/<int:pk>/',ContactDetailView.as_view(), name="detail"),
+    path('contact/create/',ContactCreateView.as_view(), name="create"),
+    path('contact/manage/',ManageContactList.as_view(), name="manage"),
+    path('contact/update/<int:pk>/',ContactUpdateView.as_view(), name="update"),
+    path('contact/delete/<int:pk>/',ContactDeleteView.as_view(), name="delete"),
+
 ]
