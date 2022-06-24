3cb3935e96e1501c1ce98a142df91ac0d10244ec
Jasmine Massey <jazminemazzey@gmail.com>
2022-06-24 11:08:28 +0530

    created views and templates

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,13 @@
+from pyexpat import model
 from django.db import models
 
-# Create your models here.
+class Contacts(models.Model):
+    first_name = models.CharField(max_length=15, help_text="John")
+    last_name = models.CharField(max_length=15, help_text="Doe")
+    ph_num = models.CharField(max_length=10, help_text="9999999999")
+
+    class Meta:
+        ordering = ['-first_name']
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.ph_num}"
