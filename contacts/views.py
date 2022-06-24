2184b62588bf4e41ea470dbf91068aa286d3257e
Jasmine Massey <jazminemazzey@gmail.com>
2022-06-24 13:45:23 +0530

    generated views for CRUD functionality

--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,39 @@
-import imp
 from .models import Contacts
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView,UpdateView,DeleteView
+from django.views.generic.detail import DetailView
+from .forms import ContactForm
 
 class IndexView(ListView):
     model = Contacts
     template_name = 'contacts/index.html'
+    context_object_name = 'index'
+
+class ContactDetailView(DetailView):
+    model = Contacts
+    template_name = 'contacts/detail.html'
+    context_object_name = 'contact'
+
+class ContactCreateView(CreateView):
+    model = Contacts
+    # fields = ['first_name', 'last_name', 'ph_num']
+    form_class = ContactForm
+    template_name = 'contacts/create.html'
+    success_url = '/'
+
+class ManageContactList(ListView):
+    model = Contacts
+    template_name = 'contacts/manage.html'
+    context_object_name = 'manage_list'
+
+class ContactUpdateView(UpdateView):
+    model = Contacts
+    form_class = ContactForm
+    pk_url_kwarg = 'pk'
+    template_name = 'contacts/update.html'
+    success_url = '/contact/manage/'
+
+class ContactDeleteView(DeleteView):
+    model = Contacts
+    pk_url_kwarg = 'pk'
+    template_name = 'contacts/delete.html'
+    success_url = '/contact/manage/'
